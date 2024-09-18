b2cec5839a52ba1114b015a43800f1a0fdd5e8d9
Nathália Salema <83145775+nathaliasalema@users.noreply.github.com>
2024-09-18 14:03:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeButtonColor(button) {
+  button.style.backgroundColor = "red";
+  button.style.color = "white";
+}
+
+function resetButtonColor(button) {
+  button.style.backgroundColor = "transparent";
+  button.style.color = "black";
+}
